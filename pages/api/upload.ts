33d5b227f32f226d49f2e7173722b73fb2e9fe20
Kacper Bajer <kkacper4354@gmail.com>
2025-02-19 20:26:58 +0100

    up

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -4,7 +4,6 @@ import fs from "fs";
 import path from "path";
 import { uploadFiles } from "@/lib/uploadFiles";
 import { UploadedFile } from "@/lib/types";
-import { revalidatePath } from "next/cache";
 
 // Konfiguracja Next.js, aby wyłączyć domyślną obsługę body parsera
 export const config = {
@@ -74,7 +73,6 @@ export default async function handler(
     }
 
     
-    revalidatePath(`/folder/${folderId}`)
     return res.status(200).json({ success: true, files: uploadedFiles });
   } catch (error) {
     console.error("Upload error:", error);
